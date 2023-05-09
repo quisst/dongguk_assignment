ad573df6751298b0b9e7ac099e6625c27592c056
quisst <supercms04@gmail.com>
2023-05-09 20:36:02 +0900

    for loop

---
+++ python/4_1.py
@@ -0,0 +1,18 @@
+s = input("Enter one string : ")
+c = input("Enter one character : ")
+cnt = 0
+for i in s:
+    if s == c:
+        cnt += 1
+print("count : {}".format(cnt))
+
+# s = input("Enter one string : ")
+# c = input("Enter one character : ")
+# cnt = 0
+# pos = []
+# for i in range(len(s)):
+#     if s[i] == c:
+#         cnt += 1
+#         pos.append(i)
+# print("count : {}".format(cnt))
+# print("{}번째에 있음".format(pos))
---
+++ python/4_2.py
@@ -0,0 +1,20 @@
+a = [90, 98, 67, 92, 76, 100, 77, 80, 95, 57, 92]
+cnt = 0
+for i in a:
+    if i >= 90:
+        cnt += 1
+print("L : {}".format(a))
+print("count : {}".format(cnt))
+
+#print("avg : {}, max : {}, min : {}".format(sum(a) / len(a), max(a), min(a)))
+
+# n = int(input("총 학생들의 수를 입력하세요 : "))
+# a = []
+# cnt = 0
+# for i in range(n):
+#     a.append(int(input("점수를 입력하세요 : ")))
+# for i in a:
+#     if i >= 90:
+#         cnt += 1
+# print("L : {}".format(a))
+# print("count : {}".format(cnt))
