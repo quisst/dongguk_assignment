986481221b055b859be7db85426a4d082ef31f14
quisst <supercms04@gmail.com>
2023-05-16 20:10:56 +0900

    5/16

--- python-univ/python_038.py
+++ python-univ/python_038.py
@@ -4,6 +4,6 @@ for i in range(5):
     arr.append(input("{} Enter word : ".format(i+1)))
 for i in arr:
     arr_len.append(len(i))
-max_pos = max(arr_len)
-print("longest word ; {}".format(arr[max_pos]))
+max_pos = arr_len.find(max(arr_len))
+print("longest word : {}".format(arr[max_pos]))
 print("length : {}".format(max(arr_len)))
